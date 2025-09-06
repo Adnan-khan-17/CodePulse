bb2115de9c4701477d29082029daa6c0141af412
Adnankhan <adnankhan7232@gmail.com>
2025-09-06 13:00:42 +0530

    second migration changes with entity relationships

--- src/app/features/blog-posts/add-blog-post/add-blog-post.component.ts
+++ src/app/features/blog-posts/add-blog-post/add-blog-post.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
-import { Blogpost } from '../models/Blog-post.model';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { pipe } from 'rxjs';
 import { AddBlogPostRequest } from '../models/add-blogpost-request.model';
 import { BlogpostServiceService } from '../services/blogpost.service.service';
 import {  Router } from '@angular/router';
+import { MarkdownComponent } from 'ngx-markdown';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-add-blog-post',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MarkdownComponent, NgIf],
   templateUrl: './add-blog-post.component.html',
-  styleUrl: './add-blog-post.component.css'
+  styleUrl: './add-blog-post.component.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class AddBlogPostComponent {
+export class AddBlogPostComponent implements OnInit {
 
   model: AddBlogPostRequest;
+  categories$? = new Observable<Category[]>;
 
-  constructor( private blogpostservice: BlogpostServiceService, private router: Router)
+  constructor( 
+    private blogpostservice: BlogpostServiceService,
+    private router: Router,
+    private categoryService: CategoryService
+  )
   {
     this.model = {
 
@@ -29,12 +38,17 @@ export class AddBlogPostComponent {
       publishedDate: new Date(),
       author: '',
       isVisible: true,
+      categories: []
       
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   OnFormSubmit()
   {
+    console.log(this.model);
     
     this.blogpostservice.addBlogPost(this.model)
     .subscribe({
